c43078e6b666d66f17bafd21cdf17a7e099e01a0
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2022-11-21 08:45:40 +0300

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let button = document.getElementById("element1");
 function openurl() {
     alert('You are now leaving the page!!');
-    window.open('https://github.com/JeffMuthunga');   
+    window.open('https://github.com/Muthunga');   
 }
 button.addEventListener("click", () => {
     openurl()
